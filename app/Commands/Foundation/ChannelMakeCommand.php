6e0df44667724d8326df6b0a27450a24a49b77d6
innoflash <innocentmazando@gmail.com>
2020-03-31 21:53:11 +0200

    prepping channel make

--- app/Commands/Foundation/ChannelMakeCommand.php
+++ app/Commands/Foundation/ChannelMakeCommand.php
@@ -4,8 +4,18 @@ namespace App\Commands\Foundation;
 
 use App\Helpers\PackageDetail;
 use Illuminate\Foundation\Console\ChannelMakeCommand as LaravelChannelMake;
+use Illuminate\Support\Str;
 
 class ChannelMakeCommand extends LaravelChannelMake
 {
     use PackageDetail;
+
+    protected function buildClass($name)
+    {
+        return str_replace(
+            'DummyUser',
+            class_basename($this->userProviderModel()),
+            parent::buildClass($name)
+        );
+    }
 }
--- app/Commands/Foundation/ModelMakeCommand.php
+++ app/Commands/Foundation/ModelMakeCommand.php
@@ -4,7 +4,6 @@ namespace App\Commands\Foundation;
 
 use App\Helpers\PackageDetail;
 use Illuminate\Foundation\Console\ModelMakeCommand as LaravelModelMake;
-use Illuminate\Support\Str;
 
 class ModelMakeCommand extends LaravelModelMake
 {
