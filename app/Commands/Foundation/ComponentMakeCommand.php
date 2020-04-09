b68a362e366ea8af12788ec97cbc1e99347ad46c
innoflash <innocentmazando@gmail.com>
2020-04-09 16:03:12 +0200

    fixed make component

--- app/Commands/Foundation/ComponentMakeCommand.php
+++ app/Commands/Foundation/ComponentMakeCommand.php
@@ -3,6 +3,7 @@
 namespace App\Commands\Foundation;
 
 use App\Helpers\PackageDetail;
+use Illuminate\Console\GeneratorCommand;
 use Illuminate\Foundation\Console\ComponentMakeCommand as LaravelComponentMake;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\File;
@@ -13,24 +14,59 @@ class ComponentMakeCommand extends LaravelComponentMake
 {
     use PackageDetail;
 
-    function writeView()
+    public function handle()
+    {
+       parent::handle();
+       $this->info('Do not forget to register your components in the the package provider.');
+    }
+
+    /**
+     * Write the view for the component.
+     *
+     * @return void
+     */
+    protected function writeView()
     {
         $view = $this->getView();
+        $path = realpath(null). $this->devPath() . 'views/'.str_replace('.', '/', 'components.'.$view);
 
-        $path = $this->devPath() . 'views/' . str_replace('.', '/', 'components.' . $view);
+        //die(realpath('/'.$this->devPath()));
+        //$path = resource_path('views').'/'.str_replace('.', '/', 'components.'.$view);
 
-        if (!$this->files->isDirectory(dirname($path))) {
-            $this->files->makeDirectory(dirname($path), 0755, true, true);
+        if (! $this->files->isDirectory(dirname($path))) {
+            $this->files->makeDirectory(dirname($path), 0777, true, true);
         }
 
         file_put_contents(
-            $path . '.blade.php',
+            $path.'.blade.php',
             '<div>
-    <!-- ' . Inspiring::quote() . ' -->
+    <!-- '.Inspiring::quote().' -->
 </div>'
         );
     }
 
+    /**
+     * Build the class with the given name.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    protected function buildClass($name)
+    {
+        if ($this->option('inline')) {
+            return str_replace(
+                'DummyView',
+                "<<<'blade'\n<div>\n    ".Inspiring::quote()."\n</div>\nblade",
+                GeneratorCommand::buildClass($name)
+            );
+        }
+
+        return str_replace(
+            'DummyView',
+            'view(\'components.'.$this->getView().'\')',
+            GeneratorCommand::buildClass($name)
+        );
+    }
     /**
      * Get the view name relative to the components directory.
      *
@@ -44,28 +80,4 @@ class ComponentMakeCommand extends LaravelComponentMake
             })
             ->implode('.');
     }
-
-    /**
-     * Build the class with the given name.
-     *
-     * @param string $name
-     * @return string
-     */
-    protected function buildClass($name)
-    {
-        if ($this->option('inline')) {
-            return str_replace(
-                'DummyView',
-                "<<<'blade'\n<div>\n    " . Inspiring::quote() . "\n</div>\nblade",
-                parent::buildClass($name)
-            );
-        }
-
-        return str_replace(
-            'DummyView',
-            'view(\'components.' . $this->getView() . '\')',
-            parent::buildClass($name)
-        );
-    }
-
 }
